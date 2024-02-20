f989d157ccdf5f7a47ea49ed343d1da6aca49e4d
reyesjl <140907345+reyeslance@users.noreply.github.com>
2024-02-21 00:20:11 +0800

    updated yaw
    ^_^

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar';
+import Navbar from './Components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './components/pages/Home';
-import HeroSection from './components/HeroSection';
-import Course1 from './components/Course1';
+import Home from './Components/pages/Home';
+import HeroSection from './Components/HeroSection';
+import Course1 from './Components/Course1';
 
 
  
